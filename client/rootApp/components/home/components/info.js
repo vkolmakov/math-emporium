16325596ba9ae35fa67df9aba8dfb5af4819bc1b
vkolmakov <vkolmakov@hotmail.com>
2016-07-05 23:12:03 -0500

    One more stylystic fix, too late

--- client/rootApp/components/home/components/info.js
+++ client/rootApp/components/home/components/info.js
@@ -6,7 +6,7 @@ export default () => (
         <div className="fa-wrap">
           <i className="fa fa-5x fa-minus-circle" aria-hidden="true"></i>
         </div>
-        <h1>Danger!</h1>
+        <h1>Here be dragons</h1>
         <p>This application is not in a production-ready state. For a fully-functional app visit: <a href="http://mathcenter.herokuapp.com">http://mathcenter.herokuapp.com</a></p>
       </div>
     </div>
