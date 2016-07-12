9b4e2455e223c23bdcbd5e216e1b896da34f6664
vkolmakov <vkolmakov@hotmail.com>
2016-07-11 21:16:07 -0500

    Refactor some constants

--- client/editingApp/constants.js
+++ client/editingApp/constants.js
@@ -1,3 +1,5 @@
+import * as constants from '../constants';
+
 export const BASE_PATH = 'edit-schedule';
 
 export const GOOGLE_CALENDAR_COLORS = [{
@@ -46,30 +48,6 @@ export const GOOGLE_CALENDAR_COLORS = [{
     color: '#e1e1e1',
 }];
 
-export const TIME_OPTIONS = [
-    [9 * 60, '09:00am'],
-    [10 * 60, '10:00am'],
-    [11 * 60, '11:00am'],
-    [12 * 60, '12:00pm'],
-    [13 * 60, '1:00pm'],
-    [14 * 60, '2:00pm'],
-    [15 * 60, '3:00pm'],
-    [16 * 60, '4:00pm'],
-    [17 * 60, '5:00pm'],
-    [18 * 60, '6:00pm'],
-    [19 * 60, '7:00pm'],
-    [20 * 60, '8:00pm'],
-].map(
-    ([value, display]) => ({ value, display })
-);
+export const TIME_OPTIONS = constants.TIME_OPTIONS;
 
-export const WEEKDAY_OPTIONS = [
-    [1, 'Monday'],
-    [2, 'Tuesday'],
-    [3, 'Wednesday'],
-    [4, 'Thursday'],
-    [5, 'Friday'],
-    [6, 'Saturday'],
-].map(
-    ([value, display]) => ({ value, display })
-);
+export const WEEKDAY_OPTIONS = constants.WEEKDAY_OPTIONS;
--- client/schedulingApp/constants.js
+++ client/schedulingApp/constants.js
@@ -1,1 +1,6 @@
+import * as constants from '../constants';
+
 export const BASE_PATH = 'schedule';
+
+export const TIME_OPTIONS = constants.TIME_OPTIONS;
+export const WEEKDAY_OPTIONS = constants.WEEKDAY_OPTIONS;
