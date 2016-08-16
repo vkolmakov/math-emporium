8fa8a92f6c19ef85fe7eedba85f3a0f14f5945c8
vkolmakov <vkolmakov@hotmail.com>
2016-08-16 11:43:54 -0500

    Make usable outside redux-form

--- client/components/select/reactSelectWrapper.js
+++ client/components/select/reactSelectWrapper.js
@@ -3,6 +3,14 @@ import Select from 'react-select';
 
 // wrapper for react-select in order to use it with redux-form
 export default ({ options, binding, multi, ...rest }) => {
+    if (!binding) {
+        return (
+            <Select options={options}
+                    multi={multi || false}
+                    {...rest} />
+        );
+    }
+
     const { onBlur, value, ...bindingProps } = binding;
 
     // arrow function is required to make onBlur work
