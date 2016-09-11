4af1af4c48570d2f5b3d5c2aa7d6f0a947a2a489
vkolmakov <vkolmakov@hotmail.com>
2016-09-11 01:18:08 -0500

    Remove warning from main page

--- client/rootApp/components/home/index.js
+++ client/rootApp/components/home/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import Hero from './components/hero';
 import Features from './components/features';
-import Info from './components/info';
 
 export default () => (
     <div>
       <Hero />
       <Features />
-      <Info />
     </div>
 );
