59a840cd35ce47687a2d58b7920851f38ea7ebc9
vkolmakov <vkolmakov@hotmail.com>
2016-09-19 18:34:01 -0500

    Blur filter dissapears on focus

--- client/rootApp/components/home/components/hero.js
+++ client/rootApp/components/home/components/hero.js
@@ -5,8 +5,18 @@ export default class Hero extends Component {
     constructor() {
         super();
         this.state = {
-            focus: true,
+            focus: false,
         };
+        this.onMouseEnter = this.onMouseEnter.bind(this);
+        this.onMouseLeave = this.onMouseLeave.bind(this);
+    }
+
+    onMouseEnter() {
+        this.setState({ focus: true });
+    }
+
+    onMouseLeave() {
+        this.setState({ focus: false });
     }
 
     render() {
@@ -16,7 +26,9 @@ export default class Hero extends Component {
                 <h1>Tutoring @ Wright</h1>
                 <h2>Study with us!</h2>
                 <div>
-                  <Link to="/schedule">Schedule appointment</Link>
+                  <Link to="/schedule"
+                        onMouseEnter={this.onMouseEnter}
+                        onMouseLeave={this.onMouseLeave}>Schedule appointment</Link>
                 </div>
               </div>
             </div>
