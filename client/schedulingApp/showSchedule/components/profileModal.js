131d0a049e98aab4699139f559dd917f8c892ef9
vkolmakov <vkolmakov@hotmail.com>
2017-01-06 16:21:05 -0600

    Promote profile modal form to component

--- client/schedulingApp/showSchedule/components/profileModal.js
+++ client/schedulingApp/showSchedule/components/profileModal.js
@@ -1,15 +1,63 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Modal from 'react-modal';
 
-import UpdateProfileForm from '../../profile/components/updateProfileForm';
+import Form from '../../../components/form/index';
 
-export default ({ onRequestClose, profile, locations, courses }) => (
-    <Modal isOpen={this.state.displayProfileModal}
+class MiniProfileForm extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            firstName: '',
+            lastName: '',
+            success: false,
+            error: false,
+        };
+    }
+
+    render() {
+        const handleSubmit = e => {
+            e.preventDefault();
+            console.log('submitted');
+        };
+
+        const config = {
+            handleSubmit,
+            title: 'Update your profile',
+            fields: [{
+                label: 'First Name',
+                input: {
+                    type: 'text',
+                    binding: {
+                        onChange: e => this.setState({ firstName: e.target.value }),
+                    },
+                },
+            }, {
+                label: 'Last Name',
+                input: {
+                    type: 'text',
+                    binding: {
+                        onChange: e => this.setState({ lastName: e.target.value }),
+                    },
+                },
+            }],
+            error: null,
+            success: false,
+        };
+
+        return (
+            <div className="edit-profile-form-wrap">
+              <Form {...config} />
+            </div>
+        );
+    }
+}
+
+export default ({ onRequestClose, onSubmitSuccess }) => (
+    <Modal isOpen={true}
+           contentLabel="Profile Modal"
            onRequestClose={onRequestClose}
            className="profile-form-modal">
-        <h2>Before we start scheduling appointments we need some more info about you...</h2>
-        <UpdateProfileForm profile={profile}
-                           locations={locations}
-                           courses={courses}
-                           submitCallback={onRequestClose}/>
+        <MiniProfileForm onSubmitSuccess={onSubmitSuccess} />
     </Modal>
 );
