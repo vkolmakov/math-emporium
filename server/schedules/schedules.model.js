5cb2b1f71ec7866723284726f09403d227fb6011
vkolmakov <vkolmakov@hotmail.com>
2017-01-11 23:23:18 -0600

    Add unique schedules per location+weekday+time

--- server/schedules/schedules.model.js
+++ server/schedules/schedules.model.js
@@ -1,7 +1,5 @@
 export default function createScheduleModel(sequelize, DataTypes) {
     const schedule = sequelize.define('schedule', {
-        // TODO: Add custom validator: make sure that there's one and only one
-        //       pair of weekday and time for a given location
         weekday: {
             type: DataTypes.INTEGER,
             allowNull: false,
@@ -13,6 +11,10 @@ export default function createScheduleModel(sequelize, DataTypes) {
         },
     }, {
         timestamps: false,
+        indexes: [{
+            unique: true,
+            fields: ['weekday', 'time', 'locationId'],
+        }],
         classMethods: {
             associate(models) {
                 schedule.belongsToMany(models.tutor, { through: 'schedule_tutor', as: 'tutors' });
