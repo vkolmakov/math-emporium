1a64886c9921987da2e9d4a1c58874e02aaf40c3
vkolmakov <vkolmakov@hotmail.com>
2017-05-11 23:52:54 -0500

    Migrate filterControls from React-Select to html select

--- client/components/filterControls.js
+++ client/components/filterControls.js
@@ -1,12 +1,35 @@
 import React from 'react';
-import Select from 'react-select';
 
-export default ({ options, currentValue, onChange, placeholder, error }) => (
-    <div className={`select-filter-wrap ${error ? 'select-filter-error' : ''}`}>
-      <Select options={(options)}
-              value={currentValue}
-              searchable={false}
-              placeholder={placeholder}
-              onChange={onChange} />
-    </div>
-);
+const handleChange = (options, event) => (onChange) =>
+      onChange(options.find(o => o.value == event.target.value));
+
+export default ({ options, currentValue, onChange, placeholder, error }) => {
+    const createClassName = (error, currentValue) => {
+        const classNames = {
+            error: 'select-filter-error',
+            empty: 'select-filter-empty',
+            default: 'select-filter-wrap',
+        };
+
+        let result = [classNames.default];
+
+        if (error) {
+            result = result.concat([classNames.error]);
+        }
+
+        if (!currentValue) {
+            result = result.concat([classNames.empty]);
+        }
+
+        return result.join(' ');
+    };
+
+    return (
+        <div className={createClassName(error, currentValue)}>
+            <select value={currentValue || 0} onChange={(event) => handleChange(options, event)(onChange)}>
+                <option value={0} disabled>{placeholder}</option>
+                {options.map(({ value, label }) => (<option value={value} key={value}>{label}</option>))}
+            </select>
+        </div>
+    );
+};
