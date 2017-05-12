9c0411a1ca56c1cbacbc36467f2c70cc03683dc8
vkolmakov <vkolmakov@hotmail.com>
2017-05-11 23:56:50 -0500

    Simplify hero banner

--- client/rootApp/components/home/components/hero.js
+++ client/rootApp/components/home/components/hero.js
@@ -1,37 +1,14 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
 
-export default class Hero extends Component {
-    constructor() {
-        super();
-        this.state = {
-            focus: false,
-        };
-        this.onMouseEnter = this.onMouseEnter.bind(this);
-        this.onMouseLeave = this.onMouseLeave.bind(this);
-    }
-
-    onMouseEnter() {
-        this.setState({ focus: true });
-    }
-
-    onMouseLeave() {
-        this.setState({ focus: false });
-    }
-
-    render() {
-        return (
-            <div className={this.state.focus ? 'hero-focus' : 'hero'}>
-              <div className="hero-content">
-                <h1>Tutoring @ Wright</h1>
-                <h2>Study with us!</h2>
-                <div>
-                  <Link to="/schedule"
-                        onMouseEnter={this.onMouseEnter}
-                        onMouseLeave={this.onMouseLeave}>Schedule appointment</Link>
-                </div>
-              </div>
+export default (props) => (
+    <div className="hero">
+        <div className="hero-content">
+            <h1>Tutoring @ Wright</h1>
+            <h2>Study with us!</h2>
+            <div>
+                <Link to="/schedule">Schedule appointment</Link>
             </div>
-        );
-    }
-}
+        </div>
+    </div>
+);
