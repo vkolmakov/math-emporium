966856700e0737a0d3354dee037152040cf0973a
vkolmakov <vkolmakov@hotmail.com>
2017-05-14 10:18:50 -0500

    Factor out forms + add styles for sign-in button

--- client/auth/components/signin.js
+++ client/auth/components/signin.js
@@ -3,13 +3,16 @@ import { OAUTH2_SIGNIN_URL } from '../constants';
 import schoolLogo from '../../assets/school-logo.png';
 
 
+// TODO: pass in selected appointment, if any, in the querystring
 export default props => (
   <div className="wrap">
     <div className="auth-form-wrap">
-      <div class="oauth2-wrap">
-        <img alt="School Logo" src={schoolLogo}/>
-        <a href={OAUTH2_SIGNIN_URL} className="oauth2-link">Sign in with your school account</a>
-      </div>
+      <a href={OAUTH2_SIGNIN_URL} className="oauth2-link">
+        <div className="oauth2-wrap">
+          <img alt="School Logo" src={schoolLogo}/>
+          <span>Sign in with your school account</span>
+        </div>
+      </a>
     </div>
   </div>
 );
