4741637520ba397c66bafb5203400e21798d4ca3
vkolmakov <vkolmakov@hotmail.com>
2017-05-20 09:05:59 -0500

    Add seed script

---
+++ server/scripts/seed.js
@@ -0,0 +1,126 @@
+import fs from 'fs';
+import path from 'path';
+import db from 'sequelize-connect';
+
+import config from '../config';
+
+
+function connect() {
+    db.discover = path.resolve(__dirname, '..');
+    db.matcher = (file) => !!file.match(/.+\.model\.js/);
+
+    return db.connect(config.db.NAME,
+                      config.db.USER,
+                      config.db.PASSWORD, {
+                          dialect: 'postgres',
+                          protocol: 'postgres',
+                          port: config.db.PORT,
+                          host: config.db.HOST,
+                          logging: false,
+                          dialectOptions: {
+                              ssl: config.IS_PRODUCTION,
+                          },
+                      });
+}
+
+function readJson(fileName) {
+    return new Promise((resolve, reject) => {
+        fs.readFile(fileName, 'utf8', (err, data) => {
+            if (err) {
+                reject(err);
+            }
+            try {
+                resolve(JSON.parse(data));
+            } catch (e) {
+                reject(e);
+            }
+        });
+    });
+}
+
+function setKey(obj, key, val) {
+    obj[key] = val;
+    return obj;
+}
+
+function zipObj(keys, vals) {
+    return keys.reduce((acc, k, idx) => setKey(acc, k, vals[idx]), {});
+}
+
+function createObject(fieldNames) {
+    return values => zipObj(fieldNames, values);
+}
+
+function processData(data) {
+    return data.values.map(createObject(data.fields));
+}
+
+function insertDataIn(model) {
+    return records => model.bulkCreate(records);
+}
+
+function zip(xs, ys) {
+    return xs.reduce((acc, x, i) => setKey(acc, i, [x, ys[i]]), []);
+}
+
+function mapPromisesInOrder(fn, vals) {
+    if (vals.length === 0) return Promise.resolve();
+    const [x, ...xs] = vals;
+    return fn(x).then(() => mapPromisesInOrder(fn, [...xs]));
+}
+
+function groupBy(keyFn, vals) {
+    return vals.reduce((acc, v) => {
+        const key = keyFn(v);
+        return key in acc
+            ? setKey(acc, key, [...acc[key], v])
+            : setKey(acc, key, [v]);
+    }, {});
+}
+
+function entries(obj) {
+    return Object.keys(obj).reduce(
+        (acc, k) => acc.concat([[k, obj[k]]]), []);
+}
+
+function addTutorCourses(Tutor) {
+    return values => {
+        const groupedByTutor = groupBy(x => x.tutorId, values);
+        const getCourseId = x => x.courseId;
+        return Promise.all(
+            entries(groupedByTutor).map(
+                ([tutorId, records]) =>
+                    Tutor.findOne({ where: { id: tutorId } })
+                    .then(tutor => tutor.setCourses(records.map(getCourseId)))));
+    };
+}
+
+function seed() {
+    const Course = db.models.course;
+    const Location = db.models.location;
+    const Tutor = db.models.tutor;
+
+    const makeFileName = body => `./server/scripts/data/${body}.json`;
+    const fileNamesPrimitives = ['locations', 'courses', 'tutors'].map(makeFileName);
+    const modelsPrimitives = [Location, Course, Tutor];
+
+    const processPair = (([fileName, model]) =>
+                         readJson(fileName)
+                         .then(processData)
+                         .then(insertDataIn(model)));
+
+    const insertPrimitives = () =>
+          mapPromisesInOrder(processPair, zip(fileNamesPrimitives, modelsPrimitives));
+
+    const insertRelations = () => {
+        const fileName = './server/scripts/data/tutor_course.json';
+
+        readJson(fileName)
+            .then(processData)
+            .then(addTutorCourses(Tutor));
+    };
+
+    insertPrimitives().then(insertRelations);
+}
+
+connect().then(seed);
