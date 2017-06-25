7c048636c83d273365d7dc69ca9553e92902c73b
vkolmakov <vkolmakov@hotmail.com>
2017-06-24 20:33:14 -0500

    Format event timestamps and sort them by time

--- client/managingApp/constants.js
+++ client/managingApp/constants.js
@@ -3,6 +3,8 @@ import * as constants from '../constants';
 export const BASE_PATH = 'manage-portal';
 export const AUTH_GROUPS = constants.AUTH_GROUPS;
 
+export const TIMESTAMP_DISPLAY_FORMAT = constants.TIMESTAMP_DISPLAY_FORMAT;
+
 export const AUTH_GROUPS_OPTIONS = Object.keys(AUTH_GROUPS).map(
     display => ({ value: AUTH_GROUPS[display], display })
 );
--- client/managingApp/events/actions.js
+++ client/managingApp/events/actions.js
@@ -1,14 +1,19 @@
 import axios from 'axios';
+import moment from 'moment';
+
+import { TIMESTAMP_DISPLAY_FORMAT } from '../constants';
 
 export const GET_EVENTS = 'GET_EVENTS';
 
 const BASE_URL = '/api/events';
 
 function processEvent(event) {
+    const _time = moment(event.createdAt);
     return {
         type: event.type,
         user: event.user.email,
-        time: event.createdAt,
+        _time, // internal moment object, will be used for sorting
+        time: _time.format(TIMESTAMP_DISPLAY_FORMAT),
         data: event.data
             ? `${event.data.location.name}: ${event.data.course.code}`
             : '',
@@ -19,7 +24,7 @@ export function getEvents() {
     return (dispatch) => axios.get(BASE_URL).then(res => {
         dispatch({
             type: GET_EVENTS,
-            payload: res.data.map(processEvent),
+            payload: res.data.map(processEvent).sort((a, b) => b._time - a._time),
         });
     });
 }
