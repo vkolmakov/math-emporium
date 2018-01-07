2b8d68c5665d6c100211929d43376cc535441a3f
Vladimir Kolmakov <vkolmakov256@gmail.com>
2018-01-07 14:43:46 -0600

    Get locations info on the home page

--- client/rootApp/components/home/index.js
+++ client/rootApp/components/home/index.js
@@ -1,13 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 import MainContentWrap from '../../../components/mainContentWrap';
+import LocationsInfo from '../../../components/locationsInfo';
 
-import Hero from './components/hero';
-import Features from './components/features';
+import { getLocations } from '../../../sharedPublicData/actions';
 
-export default () => (
-    <MainContentWrap>
-      <Hero />
-      <Features />
-    </MainContentWrap>
-);
+class Home extends Component {
+    componentDidMount() {
+        if (!this.props.locations.all.length > 0) {
+            this.props.getLocations();
+        }
+    }
+
+    render() {
+        return (
+            <MainContentWrap>
+              <LocationsInfo />
+            </MainContentWrap>
+        );
+    }
+}
+
+function mapStateToProps(state) {
+    return {
+        locations: { all: state.sharedPublicData.locations.all },
+    };
+}
+
+export default connect(mapStateToProps, { getLocations })(Home);
