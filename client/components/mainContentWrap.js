8a10394b39a3ad54367bebe89324f8ca3fc4e3ac
Vladimir Kolmakov <vkolmakov256@gmail.com>
2018-01-07 12:13:05 -0600

    Swap outline for animation for page sections

--- client/components/mainContentWrap.js
+++ client/components/mainContentWrap.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 export default ({ children }) => (
-    <div id="main" tabIndex="-1">
+    <div id="main" className="page-section" tabIndex="-1">
         {children}
     </div>
 );
