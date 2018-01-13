6d3b9875456ff5c85b1cf257e642e19bea9beb7b
Vladimir Kolmakov <vkolmakov256@gmail.com>
2018-01-13 11:34:09 -0600

    Add location contact info prompt to scheduling emails

--- server/users/users.model.js
+++ server/users/users.model.js
@@ -126,8 +126,13 @@ export default function createUserModel(sequelize, DataTypes) {
                 const user = this;
                 moment.tz.setDefault(TIMEZONE);
                 const formattedTime = time.format(TIMESTAMP_VISIBLE_FORMAT);
+
+                const contactInfo = !!location.phone || !!location.email
+                      ? `Please contact us at ${location.phone || location.email} if you have any questions for us.`
+                      : '';
+
                 const emailBodyConstructor = () =>
-                      `Your appointment for ${course.code} with ${tutor.name} on ${formattedTime} in the ${location.name} has been scheduled.`;
+                      `Your appointment for ${course.code} with ${tutor.name} on ${formattedTime} in the ${location.name} has been scheduled. ${contactInfo}`;
                 const subjectConstructor = () => `Appointment reminder: ${location.name} on ${formattedTime}`;
                 return user.sendEmail({ subjectConstructor, emailBodyConstructor });
             },
