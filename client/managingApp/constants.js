6993436b155d913533cdbe8cd68bcb0bf01ff9ab
Vladimir Kolmakov <vkolmakov256@gmail.com>
2018-01-14 16:13:05 -0600

    Display readable event time for events

--- client/managingApp/constants.js
+++ client/managingApp/constants.js
@@ -4,6 +4,7 @@ export const BASE_PATH = 'manage-portal';
 export const AUTH_GROUPS = constants.AUTH_GROUPS;
 export const EVENT_TYPES = constants.EVENT_TYPES;
 
+export const TIMESTAMP_FORMAT = constants.TIMESTAMP_FORMAT;
 export const TIMESTAMP_DISPLAY_FORMAT = constants.TIMESTAMP_DISPLAY_FORMAT;
 
 export const LATEST_EVENT_LIMIT_DEFAULT = 150;
--- client/managingApp/events/actions.js
+++ client/managingApp/events/actions.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import moment from 'moment';
 
-import { TIMESTAMP_DISPLAY_FORMAT } from '../constants';
+import { TIMESTAMP_DISPLAY_FORMAT, TIMESTAMP_FORMAT } from '../constants';
 
 export const GET_EVENTS = 'GET_EVENTS';
 
@@ -9,6 +9,7 @@ const BASE_URL = '/api/events/latest';
 
 function processEvent(event) {
     const _time = moment(event.createdAt);
+    const formatTime = (t) => moment(t, TIMESTAMP_FORMAT).format(TIMESTAMP_DISPLAY_FORMAT);
 
     return {
         id: event._id, // used to set key for each item
@@ -16,7 +17,7 @@ function processEvent(event) {
         user: event.user.email,
         time: _time.format(TIMESTAMP_DISPLAY_FORMAT),
         data: event.data
-            ? `${event.data.location.name}: ${event.data.course.code}`
+            ? `${event.data.location.name}: ${event.data.course.code} on ${formatTime(event.data.time)}`
             : '',
     };
 }
