37e54ce84573c9a91ac3bc7e14dcb496a250a4b9
Vladimir Kolmakov <vkolmakov256@gmail.com>
2018-01-14 11:13:38 -0600

    Create an action for getting public app settings

--- client/util/actions.js
+++ client/util/actions.js
@@ -1,8 +1,23 @@
+import axios from 'axios';
+
+const BASE_URL_SETTINGS = '/api/public/settings';
+
 export const ROUTE_CHANGE = 'ROUTE_CHANGE';
+export const GET_PUBLIC_SETTINGS = 'GET_PUBLIC_SETTINGS';
 
 export function routeChange(routeData) {
     return {
         type: ROUTE_CHANGE,
         payload: routeData,
+    };
+}
+
+export function getPublicApplicationSettings() {
+    return (dispatch) => {
+        return axios.get(BASE_URL_SETTINGS)
+            .then((response) => dispatch({
+                type: GET_PUBLIC_SETTINGS,
+                payload: response.data,
+            }));
     };
 }
--- client/util/reducer.js
+++ client/util/reducer.js
@@ -1,14 +1,19 @@
-import { WINDOW_RESIZE, ROUTE_CHANGE } from './actions';
-import { SCREEN_SIZE } from '../constants';
+import { ROUTE_CHANGE, GET_PUBLIC_SETTINGS } from './actions';
 
 const INITIAL_UTIL_STATE = {
     currentRouterPath: '',
+    settings: {
+        applicationMainHomePictureLink: '',
+        applicationTitle: 'math-emporium',
+    },
 };
 
 export default function utilReducer(state = INITIAL_UTIL_STATE, action) {
     switch (action.type) {
     case ROUTE_CHANGE:
         return { ...state, currentRouterPath: action.payload.path };
+    case GET_PUBLIC_SETTINGS:
+        return { ...state, settings: action.payload };
     default:
         return state;
     }
