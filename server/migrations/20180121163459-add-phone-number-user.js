c3ee9653aac9b483d12a88ef7ecfa8713c00bdbb
Vladimir Kolmakov <vkolmakov256@gmail.com>
2018-01-21 10:38:51 -0600

    Add phone number to user migration

---
+++ server/migrations/20180121163459-add-phone-number-user.js
@@ -0,0 +1,15 @@
+const USERS_TABLE_NAME = 'users';
+const PHONE_NUMBER_COLUMN_NAME = 'phoneNumber';
+
+module.exports = {
+    up: (queryInterface, Sequelize) => {
+        return queryInterface.addColumn(
+            USERS_TABLE_NAME,
+            PHONE_NUMBER_COLUMN_NAME,
+            { type: Sequelize.STRING });
+    },
+
+    down: (queryInterface, Sequelize) => {
+        return queryInterface.removeColumn(USERS_TABLE_NAME, PHONE_NUMBER_COLUMN_NAME);
+    },
+};
