874842286e25d4ee771d50f1163aaa4c2dd9d325
Vladimir Kolmakov <vkolmakov256@gmail.com>
2018-01-22 22:49:30 -0600

    Use regular modal buttons for profileModal

--- client/schedulingApp/showSchedule/components/profileModal.js
+++ client/schedulingApp/showSchedule/components/profileModal.js
@@ -22,7 +22,7 @@ class MiniProfileForm extends Component {
         };
     }
 
-    render() {
+    onFormSubmit(e) {
         function chainErrorHandler(errorHandler) {
             // this pattern is required because each step of the submission has it's own error handler
             // and it is crucial that ONLY the error handler of the failed step executes
@@ -40,56 +40,59 @@ class MiniProfileForm extends Component {
             };
         }
 
-        const onSubmit = e => {
+        if (e) {
             e.preventDefault();
-            this.setState({ success: false, error: null });
+        }
 
-            const { phoneNumber } = this.state;
+        this.setState({ success: false, error: null });
 
-            const displayLoadingModalAndGetAvailableTutors = (selectedOpenSpotInfo) => () => {
-                const { time, course, location, subject } = selectedOpenSpotInfo;
-                this.props.displayLoadingModal();
-                return this.props.getAvailableTutors({ time, course, location, subject });
-            };
+        const { phoneNumber } = this.state;
 
-            const displayErrorMessageModal = (err) => this.props.displayMessageModal({
-                message: err.data && err.data.error
-                    ? `${err.data.error}`
-                    : 'Something went wrong, please try again.',
-            });
+        const displayLoadingModalAndGetAvailableTutors = (selectedOpenSpotInfo) => () => {
+            const { time, course, location, subject } = selectedOpenSpotInfo;
+            this.props.displayLoadingModal();
+            return this.props.getAvailableTutors({ time, course, location, subject });
+        };
 
-            const setFormError = (err) => {
-                if (typeof err === 'object' && !!err.data) {
-                    // server-side error
-                    this.setState({ error: err.data.error });
-                } else if (typeof err === 'string') {
-                    this.setState({ error: err });
-                }
-            };
+        const displayErrorMessageModal = (err) => this.props.displayMessageModal({
+            message: err.data && err.data.error
+                ? `${err.data.error}`
+                : 'Something went wrong, please try again.',
+        });
 
-            const displayTutorSelectionModalOrError = (tutors) => tutors.length > 0
-                  ? this.props.displayTutorSelectionModal({ tutors })
-                  : this.props.displayMessageModal({ message: 'There are no more tutors left for this time slot.' });
-
-            if (!!phoneNumber && isPotentialPhoneNumber(phoneNumber)) {
-                this.props.updateUserProfile({ phoneNumber }).then(
-                    displayLoadingModalAndGetAvailableTutors(this.props.selectedOpenSpotInfo),
-                    chainErrorHandler(setFormError)
-                ).then(
-                    (res) => res.data,
-                    chainErrorHandler(displayErrorMessageModal)
-                ).then(
-                    displayTutorSelectionModalOrError,
-                    chainErrorHandler(displayErrorMessageModal)
-                ).catch(
-                    (err) => err.handler(err.originalError)
-                );
-            } else {
-                setFormError('Please enter a valid phone number');
+        const setFormError = (err) => {
+            if (typeof err === 'object' && !!err.data) {
+                // server-side error
+                this.setState({ error: err.data.error });
+            } else if (typeof err === 'string') {
+                this.setState({ error: err });
             }
         };
 
-        const handleSubmit = onSubmit.bind(this);
+        const displayTutorSelectionModalOrError = (tutors) => tutors.length > 0
+              ? this.props.displayTutorSelectionModal({ tutors })
+              : this.props.displayMessageModal({ message: 'There are no more tutors left for this time slot.' });
+
+        if (!!phoneNumber && isPotentialPhoneNumber(phoneNumber)) {
+            this.props.updateUserProfile({ phoneNumber }).then(
+                displayLoadingModalAndGetAvailableTutors(this.props.selectedOpenSpotInfo),
+                chainErrorHandler(setFormError)
+            ).then(
+                (res) => res.data,
+                chainErrorHandler(displayErrorMessageModal)
+            ).then(
+                displayTutorSelectionModalOrError,
+                chainErrorHandler(displayErrorMessageModal)
+            ).catch(
+                (err) => err.handler(err.originalError)
+            );
+        } else {
+            setFormError('Please enter a valid phone number');
+        }
+    };
+
+    render() {
+        const handleSubmit = this.onFormSubmit.bind(this);
         const config = {
             handleSubmit,
             title: 'Please enter your phone number',
@@ -127,13 +130,31 @@ const ConnectedMiniProfileForm = connect(mapStateToProps, {
     displayMessageModal,
     displayTutorSelectionModal,
     getAvailableTutors,
-})(MiniProfileForm);
+}, null, { withRef: true })(MiniProfileForm);
 
-export default ({ onRequestClose }) => (
-    <Modal isOpen={true}
-           contentLabel="Profile Modal"
-           onRequestClose={onRequestClose}
-           className="profile-form-modal">
-        <ConnectedMiniProfileForm />
-    </Modal>
-);
+export default class ProfileModal extends Component {
+    render() {
+        const { onRequestClose } = this.props;
+
+        const saveProfileFormRef = (profileFormRef) => {
+            this.profileFormRef = !!profileFormRef ? profileFormRef.getWrappedInstance() : null;
+        };
+
+        return (
+            <Modal isOpen={true}
+                   contentLabel="Profile Modal"
+                   onRequestClose={onRequestClose}
+                   className="profile-form-modal">
+
+              <ConnectedMiniProfileForm ref={saveProfileFormRef}/>
+
+              <div className="buttons-input-group-wrap">
+                <button onClick={onRequestClose}
+                        className="destructive">Cancel</button>
+                <button onClick={() => this.profileFormRef.onFormSubmit()}
+                  className="nondestructive">Submit</button>
+              </div>
+            </Modal>
+        );
+    }
+}
