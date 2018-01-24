3e95d65ea3c0f543e781a86ca67d093b0f703ede
Vladimir Kolmakov <vkolmakov256@gmail.com>
2018-01-23 22:52:13 -0600

    Refactor signout page structure

--- client/auth/components/signout.js
+++ client/auth/components/signout.js
@@ -4,23 +4,34 @@ import TimerMixin from 'react-timer-mixin';
 import reactMixin from 'react-mixin';
 
 import { signoutUser } from '../actions';
+
+import MainContentWrap from '../../components/mainContentWrap';
 import { OAUTH2_PROVIDER_SIGNOUT_URL } from '../constants';
+import { redirectOutside } from '../../utils';
 
 class Signout extends Component {
     componentWillMount() {
         this.props.signoutUser();
-        const redirectToOauthProviderSignout = () =>
-              window.location = OAUTH2_PROVIDER_SIGNOUT_URL;
+        const redirectToOauthProviderSignout = () => {
+            redirectOutside(OAUTH2_PROVIDER_SIGNOUT_URL);
+        };
+
         setTimeout(redirectToOauthProviderSignout, 4000);
     }
 
     render() {
         return (
-            <div className="auth-form-wrap">
-                <h1>Goodbye!</h1>
-                <h2>We will redirect you to sign out of your school account in a moment...</h2>
-                <h2>You can also click <a href={OAUTH2_PROVIDER_SIGNOUT_URL}>here</a> to sign out of your school account right away.</h2>
-            </div>
+            <MainContentWrap>
+              <div className="wrap">
+                <div className="auth-content-wrap">
+                  <div className="signout-message">
+                    <h1>Goodbye!</h1>
+                    <p>We will redirect you to sign out of your school account in a moment...</p>
+                    <p>You can also click <a href={OAUTH2_PROVIDER_SIGNOUT_URL}>here</a> to sign out of your school account right away.</p>
+                  </div>
+                </div>
+              </div>
+            </MainContentWrap>
         );
     }
 }
