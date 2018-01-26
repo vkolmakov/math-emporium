9bcc68b00acbe8559f151ce5202b43182974a518
Vladimir Kolmakov <vkolmakov256@gmail.com>
2018-01-25 22:08:39 -0600

    Refactor cache key handling implementation

--- server/services/cache.js
+++ server/services/cache.js
@@ -14,10 +14,14 @@ const _cache = {
         APP_DATA: minutes(1),
     },
 
-    keys: {
+    keyNames: {
         calendarEvents: (calendarId, startDateIsoString) =>
             `CALENDAR_EVENTS-${calendarId}-${startDateIsoString}`,
         appData: () => 'APP_DATA',
+    },
+
+    existingKeys() {
+        return memoryCache.keys();
     },
 
     get(key) {
@@ -37,34 +41,34 @@ const _cache = {
 
 const calendarEvents = {
     invalidate(calendarId) {
-        const toRemove = memoryCache.keys().filter((key) => key.includes(calendarId));
+        const toRemove = _cache.existingKeys().filter((key) => key.includes(calendarId));
         toRemove.forEach(_cache.remove);
         return toRemove;
     },
 
     put(calendarId, weekStartIsoString, events) {
         return _cache.put(
-            _cache.keys.calendarEvents(calendarId, weekStartIsoString),
+            _cache.keyNames.calendarEvents(calendarId, weekStartIsoString),
             events,
             _cache.DURATIONS.CALENDAR_EVENTS);
     },
     get(calendarId, weekStartIsoString) {
-        return _cache.get(_cache.keys.calendarEvents(calendarId, weekStartIsoString));
+        return _cache.get(_cache.keyNames.calendarEvents(calendarId, weekStartIsoString));
     },
 };
 
 const appData = {
     invalidate() {
-        return memoryCache.keys()
-            .filter((key) => key === _cache.keys.appData())
+        return _cache.existingKeys()
+            .filter((key) => key === _cache.keyNames.appData())
             .forEach(_cache.remove);
     },
     put(data) {
-        return _cache.put(_cache.keys.appData(), data, _cache.DURATIONS.APP_DATA);
+        return _cache.put(_cache.keyNames.appData(), data, _cache.DURATIONS.APP_DATA);
     },
 
     get() {
-        return _cache.get(_cache.keys.appData());
+        return _cache.get(_cache.keyNames.appData());
     },
 };
 
