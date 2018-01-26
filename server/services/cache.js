710cb9a32b929d0884c342e435525077b354219d
Vladimir Kolmakov <vkolmakov256@gmail.com>
2018-01-25 20:48:55 -0600

    Put all internal cache-related parts into a separate object

--- server/services/cache.js
+++ server/services/cache.js
@@ -2,74 +2,69 @@ import memoryCache from 'memory-cache';
 
 import { Either } from '../aux';
 
-const MESSAGES = {
-    MISS: 'CACHE_MISS',
-    INVALIDATE: 'CACHE_INVALIDATE',
-};
-
-const keys = {
-    calendarEvents: (calendarId, startDateIsoString) =>
-        `CALENDAR_EVENTS-${calendarId}-${startDateIsoString}`,
-    appData: () => 'APP_DATA',
-};
-
 const minutes = (n) => n * 60 * 1000;
 
-const DURATIONS = {
-    CALENDAR_EVENTS: minutes(1),
-    APP_DATA: minutes(1),
+const _cache = {
+    ERROR_MESSAGES: {
+        MISS: 'CACHE_MISS',
+    },
+
+    DURATIONS: {
+        CALENDAR_EVENTS: minutes(1),
+        APP_DATA: minutes(1),
+    },
+
+    keys: {
+        calendarEvents: (calendarId, startDateIsoString) =>
+            `CALENDAR_EVENTS-${calendarId}-${startDateIsoString}`,
+        appData: () => 'APP_DATA',
+    },
+
+    get(key) {
+        return Either.toEither(_cache.ERROR_MESSAGES.MISS, memoryCache.get(key));
+    },
+
+    remove(key) {
+        memoryCache.del(key);
+        return key;
+    },
+
+    put(key, value, duration) {
+        memoryCache.put(key, value, duration);
+        return value;
+    },
 };
-
-function get(key) {
-    return Either.toEither(MESSAGES.MISS, memoryCache.get(key));
-}
-
-function remove(key) {
-    memoryCache.del(key);
-    return key;
-}
-
-function put(key, value, duration) {
-    memoryCache.put(key, value, duration);
-    return value;
-}
 
 function invalidateCalendarEventsCache(calendarId) {
     const toRemove = memoryCache.keys().filter((key) => key.includes(calendarId));
-    toRemove.forEach(remove);
+    toRemove.forEach(_cache.remove);
     return toRemove;
 }
 
 function putCalendarEvents(calendarId, weekStartIsoString, events) {
-    return put(
-        keys.calendarEvents(calendarId, weekStartIsoString),
+    return _cache.put(
+        _cache.keys.calendarEvents(calendarId, weekStartIsoString),
         events,
-        DURATIONS.CALENDAR_EVENTS);
+        _cache.DURATIONS.CALENDAR_EVENTS);
 }
 
 function getCalendarEvents(calendarId, weekStartIsoString) {
-    return get(keys.calendarEvents(calendarId, weekStartIsoString));
+    return _cache.get(_cache.keys.calendarEvents(calendarId, weekStartIsoString));
 }
 
 function putAppData(data) {
-    return put(keys.appData(), data, DURATIONS.APP_DATA);
+    return _cache.put(_cache.keys.appData(), data, _cache.DURATIONS.APP_DATA);
 }
 
 function getAppData() {
-    return get(keys.appData());
+    return _cache.get(_cache.keys.appData());
 }
 
 function invalidateAppDataCache() {
-    return memoryCache.keys().filter((key) => key === keys.appData());
+    return memoryCache.keys().filter((key) => key === _cache.keys.appData());
 }
 
 export default {
-    keys,
-    get,
-    put,
-    remove,
-    DURATIONS,
-    MESSAGES,
     // calendar events
     invalidateCalendarEventsCache,
     putCalendarEvents,
