f9c736532d7435846be763d2664517ec6fbfbfe7
Vladimir Kolmakov <vkolmakov256@gmail.com>
2018-02-03 12:49:29 -0600

    Fix an error message typo

--- server/middleware/errorHandler.js
+++ server/middleware/errorHandler.js
@@ -10,6 +10,6 @@ export default (err, req, res, next) => {
     switch (err.status) {
     case 404: res.status(err.status).json(err); break;
     case 422: res.status(err.status).json(err); break;
-    default: res.status(500).json(errorMessage('An internal server error occured', 500)); break;
+    default: res.status(500).json(errorMessage('An internal server error occurred', 500)); break;
     }
 };
