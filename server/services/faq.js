2bbda25e0ed70075b3132e926e1c6df160cd9b64
Vladimir Kolmakov <vkolmakov256@gmail.com>
2018-02-11 09:39:51 -0600

    Convert markdown to html for faq

--- server/services/faq.js
+++ server/services/faq.js
@@ -1,5 +1,16 @@
+import showdown from 'showdown';
+
+function mdToHtml(text) {
+    const converter = new showdown.Converter({
+        headerLevelStart: 3,
+        noHeaderId: true,
+    });
+
+    return converter.makeHtml(text);
+}
+
 export default {
     compileToHtml(markdown) {
-        return 'Transformed markdown';
+        return mdToHtml(markdown);
     },
 };
