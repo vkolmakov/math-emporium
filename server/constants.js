2a4f2a7a457874b0b0fbded568d643c7aef50f31
Vladimir Kolmakov <vkolmakov256@gmail.com>
2018-02-25 10:06:09 -0600

    Split dev scripts into client and server explicitly and add cross-env

---
+++ server/constants.js
@@ -0,0 +1,13 @@
+const NODE_ENV_VALUE = {
+    PRODUCTION: 'production',
+    DEV: 'dev',
+};
+
+const CLI_OPTIONS = {
+    DEV_CLIENT: '-dev-client',
+};
+
+module.exports = {
+    NODE_ENV_VALUE,
+    CLI_OPTIONS,
+};
