c1751216e1f812b0545a30362d535a43a1adae8d
Vladimir Kolmakov <vkolmakov256@gmail.com>
2018-06-02 19:41:53 -0500

    Bump compression-webpack-plugin and add compression

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
+
 const webpack = require('webpack');
+const CompressionPlugin = require('compression-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
@@ -40,6 +42,13 @@ module.exports = {
         new HtmlWebpackPlugin({
             template: path.resolve('client', 'index.template.html'),
             inject: 'body',
+        }),
+        new CompressionPlugin({
+            asset: '[path].gz[query]',
+            algorithm: 'gzip',
+            test: /\.(js|css)$/,
+            minRatio: 0.8,
+            deleteOriginalAssets: true,
         }),
     ],
 
