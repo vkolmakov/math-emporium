c9790ae9e448a4e6b434a25c0b51f3ff3ed836d6
Vladimir Kolmakov <vkolmakov256@gmail.com>
2018-06-04 22:32:23 -0500

    Add AsyncComponent + stub out parent component for editing

--- client/Router.js
+++ client/Router.js
@@ -1,8 +1,38 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
-import App from './rootApp/index';
-import Home from './rootApp/components/home/index';
+import { BASE_PATH, AUTH_GROUPS } from '@client/constants';
+import RequireAuthGroup from '@client/auth/components/requireAuthGroup';
+
+import App from '@client/rootApp/index';
+import Home from '@client/rootApp/components/home/index';
+
+function AsyncComponent(getComponent) {
+    return class AsyncComponent extends Component {
+        constructor(props) {
+            super(props);
+            this.state = { Component: this.Component };
+        }
+
+        componentWillMount() {
+            if (!this.state.Component) {
+                getComponent().then((Component) => { this.setState({ Component }); });
+            }
+        }
+
+        render() {
+            const { Component } = this.state;
+            if (Component) {
+                return (<Component {...this.props} />);
+            }
+            return null;
+        }
+    };
+}
+
+const Editing = RequireAuthGroup(AUTH_GROUPS.employee)(
+    AsyncComponent(() => System.import('./editingApp/index').then((module) => module.default))
+);
 
 export default class Router extends Component {
     render() {
@@ -11,6 +41,8 @@ export default class Router extends Component {
               <div>
                 <Route path="/" component={App}></Route>
                 <Route exact path="/" component={Home}></Route>
+                <Route path="/edit-schedule" component={Editing}></Route>
+
               </div>
             </BrowserRouter>
         );
