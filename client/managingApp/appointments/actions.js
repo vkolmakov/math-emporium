f495fff153c14e6da5347f06d5ad201adb3cf0d9
Vladimir Kolmakov <vkolmakov256@gmail.com>
2018-06-16 11:29:45 -0500

    Display retreived appointments

--- client/managingApp/appointments/actions.js
+++ client/managingApp/appointments/actions.js
@@ -7,12 +7,19 @@ export const GET_APPOINTMENTS = "GET_APPOINTMENTS";
 
 const BASE_URL = "/api/admin/scheduled-appointments";
 
+function processAppointment(appointment) {
+    return {
+        ...appointment,
+        time: moment(appointment.time).format(TIMESTAMP_DISPLAY_FORMAT),
+    };
+}
+
 export function getActiveAppointments() {
     return (dispatch) => {
         return axios.get(BASE_URL).then((res) => {
             dispatch({
                 type: GET_APPOINTMENTS,
-                payload: [],
+                payload: res.data.map(processAppointment),
             });
         });
     };
