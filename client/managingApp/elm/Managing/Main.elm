7afba9ac07a5a14c7c6152747549ce5e489598eb
Vladimir Kolmakov <vkolmakov256@gmail.com>
2018-06-16 23:38:05 -0500

    Start outlining navigation for Managing

--- client/managingApp/elm/Managing/Main.elm
+++ client/managingApp/elm/Managing/Main.elm
@@ -1,35 +1,79 @@
 module Managing.Main exposing (..)
 
-import Html exposing (text)
+import Html exposing (..)
 import Navigation
+import Managing.Routes as Routes
 
 
 type Msg
     = UrlChange Navigation.Location
+    | ChangeLocation String
 
 
 type alias Model =
-    {}
+    { route : Routes.Route }
 
 
+init : Navigation.Location -> ( { route : Routes.Route }, Cmd msg )
 init location =
     let
         initialModel =
-            {}
+            { route = Routes.parseLocation location }
     in
-        update (UrlChange location) initialModel
+        ( initialModel, Cmd.none )
 
 
+navigation : Html Msg
+navigation =
+    let
+        links =
+            [ Routes.link (ChangeLocation "/manage-portal") "/manage-portal"
+            , Routes.link (ChangeLocation "/manage-portal/users") "/manage-portal/users"
+            ]
+                |> List.map (\link -> div [] [ link ])
+    in
+        div [] links
+
+
+content : Routes.Route -> Html msg
+content route =
+    case route of
+        Routes.HomeRoute ->
+            section [] [ text "home route" ]
+
+        Routes.UsersRoute ->
+            section [] [ text "users route" ]
+
+        Routes.UnknownRoute ->
+            section [] [ text "unknown route" ]
+
+
+view : { a | route : Routes.Route } -> Html Msg
 view model =
-    text "Hello from Elm"
+    main_ []
+        [ navigation
+        , content model.route
+        ]
 
 
+update :
+    Msg
+    -> { a | route : Routes.Route }
+    -> ( { a | route : Routes.Route }, Cmd msg )
 update msg model =
     case msg of
+        ChangeLocation path ->
+            ( model, Navigation.newUrl path )
+
         UrlChange location ->
-            ( model, Cmd.none )
+            let
+                nextRoute =
+                    Routes.parseLocation location
+            in
+                ( { model | route = nextRoute }, Cmd.none )
 
 
+main : Program Never { route : Routes.Route } Msg
 main =
     Navigation.program UrlChange
         { init = init
--- client/managingApp/elm/Managing/Routes.elm
+++ client/managingApp/elm/Managing/Routes.elm
@@ -1,5 +1,41 @@
 module Managing.Routes exposing (..)
 
+import Html exposing (..)
+import Html.Attributes exposing (href)
+import Html.Events exposing (onWithOptions)
+import Navigation
+import Json.Decode as Decode
+import UrlParser exposing (..)
+
 
 type Route
-    = Home
+    = HomeRoute
+    | UsersRoute
+    | UnknownRoute
+
+
+matchers : Parser (Route -> c) c
+matchers =
+    UrlParser.oneOf
+        [ UrlParser.map HomeRoute (UrlParser.s "manage-portal")
+        , UrlParser.map UsersRoute (UrlParser.s "manage-portal" </> UrlParser.s "users")
+        ]
+
+
+parseLocation : Navigation.Location -> Route
+parseLocation location =
+    case (UrlParser.parsePath matchers location) of
+        Just route ->
+            route
+
+        Nothing ->
+            UnknownRoute
+
+
+link : msg -> String -> Html.Html msg
+link msg path =
+    let
+        onClick =
+            onWithOptions "click" { stopPropagation = False, preventDefault = True } (Decode.succeed msg)
+    in
+        a [ onClick, href path ] [ text path ]
