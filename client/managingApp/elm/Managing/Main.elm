035e2ff41883392a2b611296cb20cf9f0acde6e1
Vladimir Kolmakov <vkolmakov256@gmail.com>
2018-06-24 09:57:51 -0500

    Add basic routing and navigation

--- client/managingApp/elm/Managing/Main.elm
+++ client/managingApp/elm/Managing/Main.elm
@@ -1,12 +1,17 @@
 module Managing.Main exposing (..)
 
-import Html as H exposing (Html)
+import Html as H exposing (Html, Attribute)
+import Html.Events as E
+import Html.Attributes as A
 import Managing.Styles as Styles
+import Navigation
+import UrlParser exposing (s, (</>))
+import Json.Decode as Decode
 
 
 main : Program Never Model Msg
 main =
-    H.program
+    Navigation.program BrowserLocationChange
         { init = init
         , view = view
         , update = update
@@ -19,13 +24,13 @@ main =
 
 
 type alias Model =
-    { stuff : Maybe String
+    { route : Route
     }
 
 
-init : ( Model, Cmd Msg )
-init =
-    ( Model Nothing
+init : Navigation.Location -> ( Model, Cmd Msg )
+init location =
+    ( Model (locationToRoute location)
     , Cmd.none
     )
 
@@ -35,14 +40,22 @@ init =
 
 
 type Msg
-    = ChangeStuff
+    = BrowserLocationChange Navigation.Location
+    | ChangeRoute Route
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        ChangeStuff ->
-            ( model, Cmd.none )
+        BrowserLocationChange newLocation ->
+            let
+                newRoute =
+                    locationToRoute newLocation
+            in
+                ( { model | route = newRoute }, Cmd.none )
+
+        ChangeRoute newRoute ->
+            ( model, Navigation.newUrl <| routeToPath newRoute )
 
 
 
@@ -52,8 +65,46 @@ update msg model =
 view : Model -> Html Msg
 view model =
     H.div [ Styles.mainContainerStyle ]
-        [ H.h1 [] [ H.text "Stuff" ]
+        [ viewNavigation model
+        , viewPageContent model
         ]
+
+
+viewNavigation : Model -> Html Msg
+viewNavigation model =
+    let
+        links =
+            [ linkTo HomeRoute (ChangeRoute HomeRoute) [] [ H.text "To home" ]
+            , linkTo UsersRoute (ChangeRoute UsersRoute) [] [ H.text "To users" ]
+            ]
+    in
+        H.ul [] (links |> List.map (\l -> H.li [] [ l ]))
+
+
+viewPageContent : Model -> Html msg
+viewPageContent model =
+    let
+        pageView =
+            case model.route of
+                HomeRoute ->
+                    H.text "At home route"
+
+                UsersRoute ->
+                    H.text "At users route"
+
+                UnknownRoute ->
+                    H.text "At unknown route"
+    in
+        H.div [] [ pageView ]
+
+
+linkTo : Route -> msg -> List (Attribute msg) -> List (Html msg) -> Html msg
+linkTo to msg attrs =
+    let
+        changeLocationOnClick =
+            E.onWithOptions "click" { stopPropagation = False, preventDefault = True } (Decode.succeed msg)
+    in
+        H.a ([ changeLocationOnClick, A.href <| routeToPath to ] ++ attrs)
 
 
 
@@ -63,6 +114,46 @@ view model =
 subscriptions : Model -> Sub Msg
 subscriptions model =
     Sub.none
+
+
+
+-- ROUTING
+
+
+type Route
+    = HomeRoute
+    | UsersRoute
+    | UnknownRoute
+
+
+routeToPath : Route -> String
+routeToPath r =
+    case r of
+        HomeRoute ->
+            "/manage-portal"
+
+        UsersRoute ->
+            "/manage-portal/users"
+
+        UnknownRoute ->
+            "/manage-portal"
+
+
+locationToRoute : Navigation.Location -> Route
+locationToRoute location =
+    let
+        matchers =
+            UrlParser.oneOf
+                [ UrlParser.map HomeRoute (s "manage-portal")
+                , UrlParser.map UsersRoute (s "manage-portal" </> s "users")
+                ]
+    in
+        case (UrlParser.parsePath matchers location) of
+            Just route ->
+                route
+
+            Nothing ->
+                UnknownRoute
 
 
 
