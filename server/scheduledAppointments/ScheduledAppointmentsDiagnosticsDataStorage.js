bac65e4688f1a8c109c125fa7d468f888fb9b502
Vladimir Kolmakov <vkolmakov256@gmail.com>
2018-10-18 21:41:39 -0500

    Implement diagnostic data storage

--- server/scheduledAppointments/ScheduledAppointmentsDiagnosticsDataStorage.js
+++ server/scheduledAppointments/ScheduledAppointmentsDiagnosticsDataStorage.js
@@ -1,10 +1,26 @@
+import mongoose from "mongoose";
+
+const ScheduledAppointmentDiagnostic = mongoose.model(
+    "ScheduledAppointmentDiagnostic",
+    mongoose.Schema({
+        actionName: String,
+        appointmentId: { type: Number, index: true },
+        timestamp: Date,
+        calendarState: Object,
+        derivedItems: Object,
+    })
+);
+
 export default class ScheduledAppointmentsDiagnosticsDataStorage {
     read(id) {
-        return Promise.resolve();
+        return ScheduledAppointmentDiagnostic.findOne({ appointmentId: id });
     }
 
     write(id, data) {
-        console.log("writing", { id, data });
-        return Promise.resolve();
+        const entry = new ScheduledAppointmentDiagnostic({
+            ...data,
+            appointmentId: id,
+        });
+        return entry.save();
     }
 }
