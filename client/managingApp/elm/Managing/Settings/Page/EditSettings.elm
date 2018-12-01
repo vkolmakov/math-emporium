3af3e7495a052aa0e7f29864c77e110c24e83f81
Vladimir Kolmakov <vkolmakov256@gmail.com>
2018-12-01 12:20:13 -0600

    Settings page - retry logic and check if request is taking too long

--- client/managingApp/elm/Managing/Settings/Page/EditSettings.elm
+++ client/managingApp/elm/Managing/Settings/Page/EditSettings.elm
@@ -9,7 +9,13 @@ module Managing.Settings.Page.EditSettings exposing
     )
 
 import Html as H exposing (Html)
+import Http
+import Json.Decode as Json
 import Managing.AppConfig exposing (AppConfig)
+import Managing.Request.RemoteData as RemoteData exposing (RemoteData)
+import Managing.View.DataTable as DataTable
+import Managing.View.Loading exposing (spinner)
+import Managing.View.PageError as PageError
 
 
 
@@ -18,16 +24,19 @@ import Managing.AppConfig exposing (AppConfig)
 
 type alias Settings =
     { applicationTitle : String
+    , applicationMainHomePictureLink : String
     }
 
 
 type alias Model =
-    { applicationTitle : String }
+    { appConfig : AppConfig
+    , settings : RemoteData Settings
+    }
 
 
 init : AppConfig -> Model
 init appConfig =
-    Model ""
+    Model appConfig RemoteData.Requested
 
 
 
@@ -35,16 +44,26 @@ init appConfig =
 
 
 type Msg
-    = NoOp
+    = ReceiveSettings (Result Http.Error Settings)
+    | Retry RemoteRequestItem
+    | CheckIfTakingTooLong RemoteRequestItem
+    | NoOp
 
 
 type OutMsg
     = NoOpOutMsg
 
 
+type RemoteRequestItem
+    = SettingsRequest
+
+
 initCmd : Model -> Cmd Msg
 initCmd model =
-    Cmd.none
+    Cmd.batch
+        [ getSettings
+        , RemoteData.scheduleLoadingStateTrigger (CheckIfTakingTooLong SettingsRequest)
+        ]
 
 
 update : Msg -> Model -> ( Model, Cmd Msg, Maybe OutMsg )
@@ -52,8 +71,40 @@ update msg model =
     let
         noOp =
             ( model, Cmd.none, Nothing )
+
+        initialModel =
+            init model.appConfig
     in
     case msg of
+        ReceiveSettings (Err err) ->
+            ( { model | settings = RemoteData.Error <| RemoteData.errorFromHttpError err }
+            , Cmd.none
+            , Nothing
+            )
+
+        ReceiveSettings (Ok settings) ->
+            ( { model | settings = RemoteData.Available settings }
+            , Cmd.none
+            , Nothing
+            )
+
+        CheckIfTakingTooLong SettingsRequest ->
+            case model.settings of
+                RemoteData.Requested ->
+                    ( { model | settings = RemoteData.StillLoading }
+                    , Cmd.none
+                    , Nothing
+                    )
+
+                _ ->
+                    noOp
+
+        Retry SettingsRequest ->
+            ( initialModel
+            , initCmd initialModel
+            , Nothing
+            )
+
         NoOp ->
             noOp
 
@@ -64,4 +115,44 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
-    H.div [] [ H.text "Settings" ]
+    case model.settings of
+        RemoteData.Requested ->
+            H.div [] []
+
+        RemoteData.NotRequested ->
+            H.div [] []
+
+        RemoteData.StillLoading ->
+            spinner
+
+        RemoteData.Available settings ->
+            DataTable.table
+                [ viewSettings settings
+                ]
+
+        RemoteData.Error err ->
+            PageError.viewPageError (Retry SettingsRequest) err
+
+
+viewSettings : Settings -> Html Msg
+viewSettings settings =
+    H.div [] [ H.text "Settings!" ]
+
+
+
+-- HTTP
+
+
+getSettings : Cmd Msg
+getSettings =
+    let
+        url =
+            "/api/settings"
+    in
+    Http.send ReceiveSettings (Http.get url decodeSettings)
+
+
+decodeSettings =
+    Json.map2 Settings
+        (Json.field "applicationTitle" Json.string)
+        (Json.field "applicationMainHomePictureLink" Json.string)
