f0602474697e234210f334850c1edd3462392d2c
Vladimir Kolmakov <vkolmakov256@gmail.com>
2018-11-30 21:07:51 -0600

    Supply transformed error events

--- server/services/errorEvent/errorEvent.service.js
+++ server/services/errorEvent/errorEvent.service.js
@@ -1,5 +1,20 @@
 import errorEventStorage from "./errorEventStorage";
+import { dateTime } from "../../aux";
+
+function transformErrorEvent(errorEvent) {
+    const transformedErrorEvent = {
+        userEmail: errorEvent.user && errorEvent.user.email,
+        stacktrace: errorEvent.stacktrace,
+        dataBlob: JSON.stringify(errorEvent.data),
+        code: errorEvent.type,
+        createdAtTimestamp: dateTime.toTimestamp(errorEvent.createdAt),
+    };
+
+    return transformedErrorEvent;
+}
 
 export function getAllErrorEvents() {
-    return errorEventStorage.getAll();
+    return errorEventStorage
+        .getAll()
+        .then((errorEvents) => errorEvents.map(transformErrorEvent));
 }
