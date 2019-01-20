aea789b2fa0e4d92ccaacc2f90c3fd1609280f31
Vladimir Kolmakov <vkolmakov256@gmail.com>
2019-01-19 23:42:05 -0600

    Display back-end errors appropriately

--- client/managingApp/elm/Managing/Utils/RemoteData.elm
+++ client/managingApp/elm/Managing/Utils/RemoteData.elm
@@ -10,10 +10,12 @@ module Managing.Utils.RemoteData exposing
     )
 
 import Http
+import Json.Decode as Json
 import Process
 import Task
 
 
+delayBeforeTooLong : Float
 delayBeforeTooLong =
     150
 
@@ -23,20 +25,70 @@ scheduleLoadingStateTrigger msg =
     Process.sleep delayBeforeTooLong |> Task.perform (always msg)
 
 
+type alias ServerErrorObject =
+    { message : String
+    , status : Int
+    }
+
+
 type RemoteDataError
     = RemoteDataError Http.Error
 
 
+type ErrorMessage
+    = ErrorMessage String
+
+
+type ServerError
+    = InternalError
+    | CannotProcessRequest ErrorMessage
+
+
+errorFromHttpError : Http.Error -> RemoteDataError
 errorFromHttpError httpError =
     RemoteDataError httpError
 
 
+errorToString : RemoteDataError -> String
 errorToString err =
     case err of
         RemoteDataError httpError ->
             httpErrorToString httpError
 
 
+decodeServerError : String -> ServerError
+decodeServerError errorBody =
+    let
+        decodeServerErrorObject =
+            Json.map2
+                ServerErrorObject
+                (Json.field "error" Json.string)
+                (Json.field "status" Json.int)
+
+        serverErrorObjectToServerError serverErrorObject =
+            case serverErrorObject.status of
+                422 ->
+                    CannotProcessRequest (ErrorMessage serverErrorObject.message)
+
+                _ ->
+                    InternalError
+    in
+    errorBody
+        |> Json.decodeString (decodeServerErrorObject |> Json.map serverErrorObjectToServerError)
+        |> Result.withDefault InternalError
+
+
+serverErrorToString : ServerError -> String
+serverErrorToString err =
+    case err of
+        InternalError ->
+            "Internal server error"
+
+        CannotProcessRequest (ErrorMessage message) ->
+            message
+
+
+httpErrorToString : Http.Error -> String
 httpErrorToString err =
     case err of
         Http.NetworkError ->
@@ -52,7 +104,9 @@ httpErrorToString err =
             "Request took too long"
 
         Http.BadStatus response ->
-            response.status.message
+            response.body
+                |> decodeServerError
+                |> serverErrorToString
 
 
 type RemoteData a
