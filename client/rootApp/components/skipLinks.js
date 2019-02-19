be0d4b04fd7531c0f8bae2dc815c025a07beae46
Vladimir Kolmakov <vkolmakov256@gmail.com>
2019-02-18 22:10:55 -0600

    Update skiplinks s.t. the focus indication on the main container only shows up when triggered through skip links.

--- client/rootApp/components/skipLinks.js
+++ client/rootApp/components/skipLinks.js
@@ -1,9 +1,33 @@
 import React from "react";
 
+let mainElementKeyboardFocusTimeout;
+
+/**
+ * @desc Adds styles that provide a focus indication when skip link
+ * to main content was used. This is done to avoid seeing the focus indication
+ * on regular page clicks that are not triggered through skip links.
+ */
+function onMainContentSelection() {
+    const mainContentElement = document.getElementById("main");
+
+    if (mainContentElement && !mainElementKeyboardFocusTimeout) {
+        mainContentElement.classList.add("was-keyboard-focused");
+        mainElementKeyboardFocusTimeout = window.setTimeout(
+            function removeKeyboardFocusIndicationMainContent() {
+                mainContentElement.classList.remove("was-keyboard-focused");
+                mainElementKeyboardFocusTimeout = void 0;
+            },
+            1000
+        );
+    }
+}
+
 export default () => (
     <ul className="skip-links">
         <li>
-            <a href="#main">Skip to content</a>
+            <a href="#main" onClick={onMainContentSelection}>
+                Skip to content
+            </a>
         </li>
     </ul>
 );
